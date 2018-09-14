276cac4df84f36f95b6611da5a3f895182b3d170
Pau Andrio <andriopau@gmail.com>
2018-09-14 12:07:28 +0200

    Changing to ConfigReader

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb_io",
-    version="0.0.6",
+    version="0.0.7",
     author="Biobb developers",
     author_email="pau.andrio@bsc.es",
     description="Biobb_io is the Biobb module collection to fetch data to be consumed by the rest of the Biobb building blocks.",
