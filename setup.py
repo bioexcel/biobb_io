06f336542ea778102a64ac3baa39742b4958a50c
Pau Andrio <andriopau@gmail.com>
2018-09-14 13:27:53 +0200

    increasing version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb_io",
-    version="0.0.7",
+    version="0.0.8",
     author="Biobb developers",
     author_email="pau.andrio@bsc.es",
     description="Biobb_io is the Biobb module collection to fetch data to be consumed by the rest of the Biobb building blocks.",
