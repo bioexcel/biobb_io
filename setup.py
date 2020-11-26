0fd650b3953ee2f9b0a8eca92aeaa094a961846b
gbayarri <genis.bayarri@irbbarcelona.org>
2020-11-26 15:48:36 +0100

    Bug fixes

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "Bioexcel": "https://bioexcel.eu/"
     },
     packages=setuptools.find_packages(exclude=['docs',]),
-    install_requires=['biobb_common==3.0.1', 'requests ==2.23.0'],
+    install_requires=['biobb_common==3.0.1'],
     python_requires='==3.7.*',
     classifiers=(
         "Development Status :: 3 - Alpha",
