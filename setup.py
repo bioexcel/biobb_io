7105fd6ab552e65200aa09f71454fead2ef011b1
gbayarri <genis.bayarri@irbbarcelona.org>
2023-09-06 16:09:17 +0200

    2023.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb_io",
-    version="4.0.0",
+    version="4.1.0",
     author="Biobb developers",
     author_email="pau.andrio@bsc.es",
     description="Biobb_io is the Biobb module collection to fetch data to be consumed by the rest of the Biobb building blocks.",
@@ -18,8 +18,8 @@ setuptools.setup(
         "Bioexcel": "https://bioexcel.eu/"
     },
     packages=setuptools.find_packages(exclude=['docs', 'test']),
-    install_requires=['biobb_common==4.0.0'],
-    python_requires='>=3.7,<3.10',
+    install_requires=['biobb_common==4.1.0'],
+    python_requires='>=3.8',
     entry_points={
         "console_scripts": [
             "alphafold = biobb_io.api.alphafold:main",
@@ -40,9 +40,9 @@ setuptools.setup(
     },
     classifiers=(
         "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
