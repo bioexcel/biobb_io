63eb0d2c682b2568703525c2df234f12526f5f27
gbayarri <genis.bayarri@irbbarcelona.org>
2024-04-30 16:11:20 +0200

    Added citation and fair badge

--- biobb_io/docs/source/conf.py
+++ biobb_io/docs/source/conf.py
@@ -67,7 +67,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'biobb_io'
-copyright = u'2023, Bioexcel Project'
+copyright = u'2024, Bioexcel Project'
 author = u'Bioexcel Project'
 
 # The version info for the project you're documenting, acts as replacement for
